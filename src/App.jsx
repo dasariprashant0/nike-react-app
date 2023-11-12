5879cef68d12fbe51fb4cbb9f075414e06002b35
Prashant Dasari <dasariprashant0@gmail.com>
2023-11-12 16:56:17 +0530

    initial setup of the project with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">Special Offers</section>
+      <section className="bg-pale-blue padding">Customer Reviews</section>
+      <section className="padding-x py-16 sm:py-32 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  );
+}
+
+export default App

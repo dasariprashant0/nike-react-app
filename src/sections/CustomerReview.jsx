b4043b862497ebb8c1f2dff685b9234869c0fc7f
Prashant Dasari <dasariprashant0@gmail.com>
2023-11-16 17:08:06 +0530

    Created CustomerReview section and ReviewCard Component with responsiveness.

--- src/sections/CustomerReview.jsx
+++ src/sections/CustomerReview.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { ReviewCard } from "../components";
+import { reviews } from "../constants";
 
 const CustomerReview = () => {
   return (
-    <div>CustomerReview</div>
-  )
-}
+    <section className="max-container">
+      <h3 className="text-4xl font-bold font-palanquin text-center">
+        What Our
+        <span className="text-coral-red"> Cusomers </span>
+        Say?
+      </h3>
+      <p className="text-center m-auto mt-4 max-w-lg info-text">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
 
-export default CustomerReview
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review, index) => (
+          <ReviewCard
+            key={index}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default CustomerReview;

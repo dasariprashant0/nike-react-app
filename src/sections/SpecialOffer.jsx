e342992d67b917c704895785276e462db24f069d
Prashant Dasari <dasariprashant0@gmail.com>
2023-11-16 16:54:39 +0530

    Created SpecialOffer section with responsiveness.

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import { Button } from "../components";
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <section className="max-container flex justify-between items-center max-xl:flex-col-reverse gap-10">
+      <div className="flex-1">
+        <img
+          src={offer}
+          alt="Shoe Promotion"
+          width={773}
+          height={687}
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-col flex-1">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">Special </span>
+          Offer
+        </h2>
+        <p className="mt-4 info-text">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="mt-6 info-text">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="mt-11 flex gap-4 flex-wrap">
+          <Button label={"Shop Now"} iconURL={arrowRight} />
+          <Button
+            label={"Learn more"}
+            backgroundColor={"bg-white"}
+            borderColor={"border-slate-gray"}
+            textColor={"text-slate-gray"}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
